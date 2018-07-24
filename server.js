c74dbd3b3ee78f3758e818ac3fb659f197a74cdc
Zachary Boyd <zachary@zacharyboyd.nyc>
2018-07-24 13:10:35 -0400

    fixes issue with static resources

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 let app = express();
 
 app.use('/', express.static(`${__dirname}/htdocs`));
+app.use('/data', express.static(`${__dirname}/data`));
+app.use('/lib/blackjack.js', express.static(`${__dirname}/blackjack.js`));
+app.use('/lib/jquery.min.js', express.static(`${__dirname}/node_modules/jquery/dist/jquery.min.js`));
+app.use('/lib/chance.min.js', express.static(`${__dirname}/node_modules/chance/dist/chance.min.js`));
 
 let port = Number(process.env.PORT) || 8000;
 
